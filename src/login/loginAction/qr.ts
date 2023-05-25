887b11d1ab41d2f572ad48d5945e621aacd7eced
Pcrab <pcrab@pcrab.xyz>
2023-05-25 22:13:03 +0800

    docs: add jsdoc

--- src/login/loginAction/qr.ts
+++ src/login/loginAction/qr.ts
@@ -3,15 +3,35 @@ import qrcode from "qrcode-terminal";
 
 const generateQrCodeUrl = "https://passport.bilibili.com/x/passport-login/web/qrcode/generate";
 
+/**
+ * {@link https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/login/login_action/QR.md#web%E7%AB%AF%E6%89%AB%E7%A0%81%E7%99%BB%E5%BD%95 | 文档}
+ */
 type GenerateQrcodeResponse = Response & {
     message: string;
     ttl: number;
     data: {
+        /**
+         * 二维码内容（登陆页面 url）
+         *
+         * 将字段内容转换为二维码使用 app 扫码
+         */
         url: string;
+        /**
+         * 扫码登录密钥
+         *
+         * 恒为 32 字符
+         */
         qrcode_key: string;
     };
 };
 
+/**
+ * 申请二维码
+ *
+ * ---
+ *
+ * {@link https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/login/login_action/QR.md#web%E7%AB%AF%E6%89%AB%E7%A0%81%E7%99%BB%E5%BD%95 | 文档}
+ */
 const generateQrCode = async (): Promise<GenerateQrcodeResponse> => {
     const response = await fetch(generateQrCodeUrl);
     const result = (await response.json()) as GenerateQrcodeResponse;
@@ -21,17 +41,49 @@ const generateQrCode = async (): Promise<GenerateQrcodeResponse> => {
 
 const pollQrcodeUrl = "https://passport.bilibili.com/x/passport-login/web/qrcode/poll";
 
+/**
+ * {@link https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/login/login_action/QR.md#%E6%89%AB%E7%A0%81%E7%99%BB%E5%BD%95web%E7%AB%AF | 文档}
+ */
 type PollQrcodeResponse = Response & {
     message: string;
     data: {
+        /**
+         * 登录页面 url
+         */
         url: string;
+        /**
+         * 刷新 token
+         */
         refresh_token: string;
+        /**
+         * 登陆时间戳
+         *
+         * 未登录时为 `0`，单位为毫秒
+         */
         timestamp: number;
+        /**
+         * 登录状态码
+         *
+         * - `0` 登录成功
+         * - `86038` 二维码已失效
+         * - `86090` 二维码已扫码，等待确认
+         * - `86101` 未扫码
+         */
         code: 0 | 86038 | 86090 | 86101;
+        /**
+         * 扫码状态信息
+         */
         message: string;
     };
 };
 
+/**
+ * 扫码登录
+ *
+ * ---
+ *
+ * {@link https://github.com/SocialSisterYi/bilibili-API-collect/blob/master/docs/login/login_action/QR.md#%E6%89%AB%E7%A0%81%E7%99%BB%E5%BD%95web%E7%AB%AF | 文档}
+ */
 const pollQrcode = async (qrcodeKey: string): Promise<PollQrcodeResponse> => {
     const response = await fetch(`${pollQrcodeUrl}?qrcode_key=${qrcodeKey}`);
     const result = (await response.json()) as PollQrcodeResponse;
