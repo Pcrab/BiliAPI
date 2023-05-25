f7c957d363ed0f5b0e0268a0f754684912a91441
Pcrab <pcrab@pcrab.xyz>
2023-05-25 22:22:31 +0800

    refactor: better export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export * from "./login";
+export * as login from "./login";
--- src/login/index.ts
+++ src/login/index.ts
@@ -1,2 +1,2 @@
-export * from "./loginAction";
-export * from "./loginInfo";
+export * as loginAction from "./loginAction";
+export * as loginInfo from "./loginInfo";
